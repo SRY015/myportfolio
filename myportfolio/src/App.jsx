cbc1f4e53f68550dc18c356fd007973dae0cc0c7
Sandip Kumar Das. <sandip@pravaahconsulting.com>
2024-07-19 11:55:27 +0530

    Initial setup

---
+++ myportfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-3xl">This is a testing ....</p>
+    </>
+  );
+}
+
+export default App;
